31dc696267bfffddd44aa0538edffef2fd3fba7f
Malek Azadegan <azadeganmalek@gmail.com>
2021-01-14 23:30:11 -0500

    started Matrix initialization & showSelf

--- Main.java
+++ Main.java
@@ -2,10 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	Fraction fraction1 = new Fraction(5, 8);
-	Fraction fraction2 = new Fraction(1, 5);
-	Fraction sum = fraction1.add(fraction2);
-	System.out.println("sum: " + sum.getString());
-
+        Matrix m = new Matrix(3, 4);
+        m.showSelf();
     }
 }
--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,33 @@
-public class Matrix {
+import java.util.Arrays;
 
+public class Matrix {
+    int rows;
+    int columns;
+    Fraction[][] data;
+
+    Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.data = new Fraction[rows][columns];
+
+        for(int row = 0; row < this.rows; row++) {
+            for(int col = 0; col < this.columns; col++) {
+                data[row][col] = new Fraction(col, 3);
+            }
+        }
+    }
+
+    public void showSelf() {
+        String[][] stringRep = new String[this.rows][this.columns];
+        // place string representation of fractions in stringRep
+        for(int row = 0; row < this.rows; row++) {
+            for(int col = 0; col < this.columns; col++) {
+                stringRep[row][col] = this.data[row][col].getString();
+            }
+        }
+        // print rows
+        for(int row = 0; row < this.rows; row++) {
+            System.out.println(Arrays.toString(stringRep[row]));
+        }
+    }
 }
